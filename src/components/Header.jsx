a27f751916716fd6e1b8aac61204ef7aeec1308e
Ilya <QwipeKun@users.noreply.github.com>
2021-10-15 21:51:31 +0300

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () =>{
+    return(
+        <header className = 'header'>
+        <img src ='https://clipart-best.com/img/chrome-logo/chrome-logo-clip-art-32.png' className = "logo"></img>
+      </header>
+    )
+}
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+const Profile = () => {
+    return(
+        <div className = 'Content'>
+        <div>
+          <img src = 'https://pbs.twimg.com/profile_banners/1173848757470785536/1569306015/1500x500'></img>
+        </div>
+        <div>
+          ava 
+        </div>
+        <div>
+          post
+            <div>
+              new post
+            </div>
+            <div>
+              <div>
+                post 1
+              </div>
+              <div>
+                post 2
+              </div>
+              <div>
+                post 3
+              </div>
+            </div>
+        </div>
+      </div>
+    )
+}
+
+export default Profile
