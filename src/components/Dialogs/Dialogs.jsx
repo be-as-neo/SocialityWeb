2e8035bb382020e13a44d8fd1e5e164caffc948b
QwipeKun <moonsov@yandex.ru>
2021-10-21 01:01:06 +0300

    add meber dialog

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,22 +1,23 @@
+import { NavLink } from 'react-router-dom'
 import s from'./Dialogs.module.css'
 const Dialogs = () => {
     return (
         <div className ={s.dialogs}>
             <div className = {s.dialogsItems}>
                 <div className = {s.dialog + ' ' + s.active}>
-                    Ivan
+                   <NavLink to = "/dialogs/1" activeClassName={s.activeLink}>Ivan</NavLink>
                 </div>
                 <div className = {s.dialog}>
-                    Ekaterina
+                    <NavLink to = "/dialogs/2" activeClassName={s.activeLink}>Ekaterina</NavLink>
                 </div>
                 <div className = {s.dialog}>
-                    Denis
+                    <NavLink to = "/dialogs/3" activeClassName={s.activeLink}> Denis</NavLink>
                 </div>
                 <div className = {s.dialog}>
-                    Vitaliy
+                    <NavLink to = "/dialogs/4" activeClassName={s.activeLink}>Oleg</NavLink>
                 </div>
                 <div className = {s.dialog}>
-                    Misha
+                    <NavLink to = "/dialogs/5" activeClassName={s.activeLink}>  Misha</NavLink>
                 </div>
             </div>
             <div className = {s.messages}>
