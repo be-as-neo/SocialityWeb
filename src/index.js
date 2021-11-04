bc99d2076a481067ef57d491ca1c39fe27266430
QwipeKun <moonsov@yandex.ru>
2021-11-04 15:18:42 +0300

    Test OOP

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import reportWebVitals from './reportWebVitals';
-import state, { subscribe } from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import {addNewMessage, addPost, updateNewMessage, updateNewPostText} from './redux/state'
 
 // addPost('SumuraiJs');
 let rerenderEntireTree = (state) => {
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App state = {state} addPost = {addPost} updateNewPostText = {updateNewPostText} addNewMessage = {addNewMessage} updateNewMessage = {updateNewMessage}/>
+    <App state = {state} addPost = {store.addPost.bind(store)} 
+        updateNewPostText = {store.updateNewPostText.bind(store)} 
+        addNewMessage = {store.addNewMessage.bind(store)} 
+        updateNewMessage = {store.updateNewMessage.bind(store)}/>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 }
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 reportWebVitals();
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,77 +1,85 @@
-let rerenderEntireTree = () =>{
-    console.log('state chenge')
-}
-let state = {
-    profilePage:{
-        postData:[ 
-            {id: 1, message: 'Love you!!!', likesCount: 16},
-            {id: 2, message: 'You my best friend!', likesCount: 17},
-            {id: 3, message: 'Hello, how are you?', likesCount: 87, avatar:'https://www.ejin.ru/wp-content/uploads/2018/10/1202399791_preview_15034743_1798112143788452_7709561090543190016_n.jpg'},
-        ],
-        newPostText: ''
-},
+let store = {
+    _state:{
+        profilePage:{
+            postData:[ 
+                {id: 1, message: 'Love you!!!', likesCount: 16},
+                {id: 2, message: 'You my best friend!', likesCount: 17},
+                {id: 3, message: 'Hello, how are you?', likesCount: 87, avatar:'https://www.ejin.ru/wp-content/uploads/2018/10/1202399791_preview_15034743_1798112143788452_7709561090543190016_n.jpg'},
+            ],
+            newPostText: ''
+    },
+    
+        messagesPage: {
+            messagesData: [    
+                {id: 1, message: 'hi'},
+                {id: 2, message: 'is'},
+                {id: 3, message: 'yo'},
+                {id: 4, message: 'name'},
+                {id: 5, message: 'Hi'},
+                {id: 6, message: 'How are you?'},
+            ],
+            newMessage: '',
+            dialogsData:[    
+                {id: 1, name: 'Ivan'},
+                {id: 2, name: 'Ekaterina'},
+                {id: 3, name: 'Danil'},
+                {id: 4, name: 'Vitalya'},
+                {id: 5, name: 'Misha'},
+                {id: 6, name: 'Oleg'},
+            ],
+    },
+    
+        sideBarPage:{
+            sideBarData: [
+                {id: 1, name: 'Ivan'},
+                {id: 2, name: 'Ekaterina'},
+                {id: 3, name: 'Danil'}
+            ]
+        }
+    },
+    getState(){
+        return this._state;
+    },
+    _callSubscriber(){
+        console.log('state chenge')
+    },
+    addPost(){
+        let newPost = {
+            id: 7,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        }
+    
+        this._state.profilePage.postData.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText){
+        this._state['profilePage']['newPostText'] = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer){
+        this._callSubscriber = observer
+    },
+    addNewMessage(){
+        let newMessage = {
+            id: 7,
+            message: this._state.messagesPage.newMessage
+        }
+        this._state.messagesPage.messagesData.push(newMessage);
+        this._state.messagesPage.newMessage = '';
+        this._callSubscriber(this._state)
+    },
+    updateNewMessage(newMessageText){
+        this._state.messagesPage.newMessage = newMessageText;
+        this._callSubscriber(this._state)
+    },
 
-    messagesPage: {
-        messagesData: [    
-            {id: 1, message: 'hi'},
-            {id: 2, message: 'is'},
-            {id: 3, message: 'yo'},
-            {id: 4, message: 'name'},
-            {id: 5, message: 'Hi'},
-            {id: 6, message: 'How are you?'},
-        ],
-        newMessage: '',
-        dialogsData:[    
-            {id: 1, name: 'Ivan'},
-            {id: 2, name: 'Ekaterina'},
-            {id: 3, name: 'Danil'},
-            {id: 4, name: 'Vitalya'},
-            {id: 5, name: 'Misha'},
-            {id: 6, name: 'Oleg'},
-        ],
-},
-
-    sideBarPage:{
-        sideBarData: [
-            {id: 1, name: 'Ivan'},
-            {id: 2, name: 'Ekaterina'},
-            {id: 3, name: 'Danil'}
-        ]
-    }
-}
-export const addPost = () => {
-    let newPost = {
-        id: 7,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    }
-
-    state.profilePage.postData.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-} 
-
-export const updateNewPostText = (newText) => {
-    state['profilePage']['newPostText'] = newText;
-    rerenderEntireTree(state);
 }
 
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer
-}
 
-export const addNewMessage = () =>{
-    let newMessage = {
-        id: 7,
-        message: state.messagesPage.newMessage
-    }
-    state.messagesPage.messagesData.push(newMessage);
-    state.messagesPage.newMessage = '';
-    rerenderEntireTree(state)
-}
-export const updateNewMessage = (newMessageText) => {
-    state.messagesPage.newMessage = newMessageText;
-    rerenderEntireTree(state)
-}
-export default state
+
+window.store = store;
+
+export default store
 
